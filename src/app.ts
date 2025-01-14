6bdc87fd6133db83ad053eda4cf2e126527ca055
Samo-Keross <samojit.paul@keross.com>
2025-01-14 17:42:26 +0530

    Added Google oauth

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,18 @@ import logger from './utils/logger';
 import routes from './routes';
 import authRoutes from './routes/authRoutes';
 
+const googleAuthRouter = require('./auth/google.route');
+
+const session = require('express-session');
+const passport = require('passport');
+require('./config/passport.config')(passport); // This loads the passport configuration
+
+
+
 const app = express();
+
+// console.log("googleAuthrouter",googleAuthRouter)
+// console.log("authRoutes",authRoutes)
 
 app.use(express.json());
 const corsOptions = {
@@ -16,12 +27,25 @@ app.use(cors(corsOptions));
 //console.log(process.env);
 
 const port = process.env.PORT || 8000;
+app.use(session({
+  secret: 'secret', 
+  resave: false,
+  saveUninitialized: true,
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/oauth',googleAuthRouter);
 app.listen(port, async () => {
   logger.info(`App is running in port ${port}`);
 
-  await connect();
+  //await connect();
 
   routes(app);
+  // app.get("/", (req, res) => {
+  //   res.send("<a href='/auth/google'>Login with Google</a>");
+  // });
   app.use('/auth', authRoutes);
+  
 });
