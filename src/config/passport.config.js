74cdb62a812f70d31ada8d44acd9e8bad8628f2f
soumyadeepkeross <soumyadeep.sinha@keross.com>
2025-01-16 10:44:54 +0530

    changes_16.01.2025

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -14,22 +14,18 @@ module.exports = (passport) => {
         callbackURL: "http://localhost:8000/oauth/google/callback"
     },
         function (accessToken, refreshToken, profile, cb) {
-            console.log("Profile", profile);
-            User.findOne({ _id: profile.id }, async (err, user) => {
-                console.log("user", user);
+            console.log("profile", profile);
+            User.findOne({ email: profile.email }, async (err, user) => {
                 if (err) return cb(err);
                 if (!user) {
                     user = await User.create({
                         oauthId: profile.id,
-                        name: profile.displayName,
+                        username: profile.displayName,
                         email: profile.emails[0].value,
                         avatar: profile.photos[0].value
                     });
                 }
-                console.log("new user created", user);
-                // const token = jwt.sign({ id: user._id, username: user.username }, secretKey);
-                // res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
-                // res.json({ token });
+                console.log("new user created" , user)
                 return cb(null, user);
             });
         }));
